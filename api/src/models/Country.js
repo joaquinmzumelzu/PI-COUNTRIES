5d0bbb4eabd8be88311423dd6e62b8877a045e52
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2022-06-18 18:20:44 -0400

    MODEL COUNTRY first changes

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,43 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    //-------------------------------------- ---> OBLIGATORIA
+    ID : {
+      type: DataTypes.STRING(5),
+      primaryKey:true,
+      allowNull: false,
+    },
+    //-------------------------------------- ---> OBLIGATORIA
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    //-------------------------------------- ---> OBLIGATORIA
+    img : {
+      type : DataTypes.TEXT,
+      allowNull: false,
+    },
+    //-------------------------------------- ---> OBLIGATORIA
+    continent: {
+      type : DataTypes.STRING,
+      allowNull: false,
+    },
+    //--------------------------------------
+    capitalCity : {
+      type : DataTypes.STRING,
+      allowNull: false,
+    },
+    //--------------------------------------
+    subRegion : {
+      type : DataTypes.STRING
+    },
+    //--------------------------------------
+    area :{
+      type : DataTypes.INTEGER
+    },
+    //--------------------------------------
+    population : {
+      type: DataTypes.INTEGER
+    }
   });
 };
