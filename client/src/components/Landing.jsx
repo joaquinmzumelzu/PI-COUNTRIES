cba4cc1a29be20b9b6a6bc545e6a335f43d28265
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2022-06-21 02:23:12 -0400

    Landing Page- background- button with Link Working

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,11 +1,16 @@
 //
+import { Link } from 'react-router-dom'
 import s from '../styles/Landing.module.css'
 //
 
 export default function Landing (){
     return (
         <div className={s.div}>
-          <h1>HENRY COUNTRIES</h1>
+          <h1 className={s.h1}>HENRY COUNTRIES</h1>
+
+          <Link className={s.button} to='/countries'>
+            <button >Get Started</button>
+          </Link>
         </div>
     )
 }
