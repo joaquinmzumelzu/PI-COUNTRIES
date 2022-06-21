490006c2aa7f982a411692f7ade5e1e05776758e
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2022-06-21 00:17:03 -0400

    Estructura lista para comenzar a trabajar en la parte del front-end

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
+ ,
+ 
   document.getElementById('root')
 );
 
