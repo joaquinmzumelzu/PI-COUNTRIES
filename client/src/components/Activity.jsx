d8a47adb8680a6670730f656824a9ff0e822cc18
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2022-07-02 02:46:57 -0400

    FORM CASI TERMINADO

--- client/src/components/Activity.jsx
+++ client/src/components/Activity.jsx
@@ -11,47 +11,96 @@ export default function Activity (){
     let sorted = [...allCountries].sort((a, b) => a.name.localeCompare(b.name))
 
     const [actCountries, setActCountries] = useState([])
+    const [form, setForm] = useState({
+      name : '',
+      duration: '',
+      difficulty: '',
+      season: '',
+      countries: [] 
+    })
 
-    function handleCountries(e){
+    const[errors,setErrors] = useState({})
+
+//-------------------------------------------------------------------------------
+    function nameToState(e){
+      e.preventDefault()
+      setForm({...form,name:e.target.value.replace(/[^a-zA-ZñÑ\s]/gi, '')})
+    }
+
+    function durationToState(e){
+      e.preventDefault()
+      setForm({...form,duration:Number(e.target.value.replace(/\D/g, ''))})
+    }
+
+    function difficultyToState(e){
+      e.preventDefault()
+      setForm({...form, difficulty:Number(e.target.value)})
+    }
+
+    function seasonToState(e){
+      e.preventDefault()
+      setForm({...form, season:e.target.value.toLowerCase()})
+    }
+
+//-------------------------------------------------------------------------------    
+
+
+      function handleCountries(e){
       e.preventDefault()
       let countrie = allCountries.find(c => c.name === e.target.value)
       
       let check = actCountries.find(c => c.name === countrie.name)
       if(check) return
 
-      setActCountries([...actCountries, {name:countrie.name,img:countrie.img}])
+      setActCountries([...actCountries, {name:countrie.name, id: countrie.ID}]) 
+      setForm({...form, countries:[...form.countries,{name:countrie.name, id: countrie.ID}]})
     }
     
     function deleteLabel (e){
       e.preventDefault()
       setActCountries(actCountries.filter(c => c.name !== e.target.value ))
+      setForm({...form,countries: form.countries.filter(c => c.name !== e.target.value )})
     }
-    // useEffect(() => {
-      
 
-       
-    // } 
-    // ,[actCountries])
+ //-------------------------------------------------------------------------------   
+
+    function validate(){
+      let errors = []
+      if(!form.name) errors.push('Activity name is required');
+      if(!form.duration) errors.push('Activity duration is required');
+      if(!form.difficulty) errors.push('Activity difficulty is required');
+      if(!form.season) errors.push('Activity season is required');
+      if(!form.countries.length) errors.push('At least 1 countrie is required');
+      
+      return errors;
+    }
+
+    function postear (e) {
+      e.preventDefault();
+      let val = validate(); 
+      val = val.join('\n')
+      if(val) alert(val)
+    }
 
     return (
         <div className={s.div}>
-          <form>
+          <form onSubmit={postear}>
 
             <div className={s.name}>
               <p>Activity name</p>  
-              <input></input>
+              <input value={form.name} onChange={nameToState} type='text' required ></input>
               <p>llena este espacio amigo</p>
             </div>
             
             <div className={s.duration}>
               <p>Activity duration (minutes)</p>  
-              <input></input>
+              <input value={form.duration} onChange={durationToState} type='text' required ></input>
               <p>llena este espacio amigo</p>
             </div>
 
             <div className={s.difficulty}>
-              <select>
-                <option disabled='' value="Select option">Select activity difficulty</option>
+              <select defaultValue="Select option" onChange={difficultyToState}>
+                <option disabled value="Select option">Select activity difficulty</option>
                 <option value="1">1 - Easy</option>
                 <option value="2">2 - Upper Easy</option>
                 <option value="3">3 - Medium</option>
@@ -61,8 +110,8 @@ export default function Activity (){
             </div>
 
             <div className={s.season}>
-              <select>
-              <option disabled='' value="Select option">Select activity season</option>
+              <select defaultValue="Select option" onChange={seasonToState}>
+              <option disabled value="Select option">Select activity season</option>
                 <option value="Summer">Summer</option>
                 <option value="Winter">Winter</option>
                 <option value="Spring">Spring</option>
@@ -72,8 +121,8 @@ export default function Activity (){
             </div>
 
             <div className={s.countries}>
-              <select onChange={handleCountries}>
-              <option disabled="" value="Select option">Select activity season</option>
+              <select defaultValue="Select option" onChange={handleCountries}>
+              <option disabled value="Select option">Select activity season</option>
                 {sorted?.map(e => {
                     return <option key={e.name}>{e.name}</option>
                 })}
